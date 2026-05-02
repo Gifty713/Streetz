a68ecf3fc803bfb3e733630526d7008119646e61
Gifty713 <giftj5584@gmail.com>
2026-05-02 10:52:20 +0100

    recent commit

---
+++ backend/src/config/cloudinary.js
@@ -0,0 +1,16 @@
+import {v2 as cloudinary} from "cloudinary";
+cloudinary.config({
+    cloud_name:"dnnmxbdxx",
+    api_key: "117243553764536",
+    api_secret: "pOrzWY92HABKs6ByF71DJLjOhC0"
+})
+
+const upholdTest =async()=>{
+    try {
+        const result = await cloudinary.uploader.upload("../middleware/jacket.jpg");
+        console.log(result);
+    } catch (error) {
+        console.log(error)
+    }
+}
+upholdTest()
--- backend/src/controllers/productController.js
+++ backend/src/controllers/productController.js
@@ -1,4 +1,20 @@
-const 
+import { Product } from "../models/productModel";
+// Function to Create product
+const addProduct = async(req, res)=>{
+    try {
+        const {name, description, price, imageURL} = req.body;
+        // verification
+        if(!name || !description || !price || !imageURL)return res.status(400).json({message:"All fields are required, thank you."});
+        // add product to mongodb
+        const addProduct = Product.create({name, description, price, imageURL});
+        
+
+    } catch (error) {
+        res.status(500).json({message:"Internal Server Error"});
+    }
+}
+
+// Function to get all products available
 const getProduct =async(req, res)=>{
     try {
         
@@ -6,3 +22,9 @@ const getProduct =async(req, res)=>{
         res.status(500).json({message: "Internal Server Error"});
     }
 }
+
+// Function to get a particular product based on category/ID
+
+// Function to delete a product from database
+
+// Function to update the product
