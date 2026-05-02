975d7a5d070e6e44f8786bf9d828a2b05b1e9e23
Gifty713 <giftj5584@gmail.com>
2026-05-02 06:38:26 +0100

    recent commit

---
+++ backend/src/config/database.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+const connectDB = async()=>{
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}`);
+        console.log(`Connection successful! ${connectionInstance.connection.host}` );
+    } catch (err) {
+        console.error("Error in connecting database", err);
+        process.exit(1);
+    }
+} 
+export default connectDB;
