2d70f677234070bcf2b3b8c6678b13e8f47185e4
Gifty713 <giftj5584@gmail.com>
2026-05-17 11:24:57 +0100

    recent commit

--- frontend/components/Component/ProductDescription.jsx
+++ frontend/components/Component/ProductDescription.jsx
@@ -12,57 +12,76 @@ import RemoveIcon from "@mui/icons-material/Remove";
 const ProductDescription=()=>{
     const {id} = useParams();
     const [oneProduct, setOneProduct] = useState("");
-    useEffect(()=>{
-        const fetchOne=async()=>{
-            try {
-                const respond = await fetch(`http://localhost:4000/api/v1/admin/getoneproduct/${id}`);
-                const data = await respond.json();
-                await setOneProduct(data);             
-            } catch (error) {
-                console.log("Error in fetching content.");
-            }
+    const fetchOne=async()=>{
+        try {
+            const respond = await fetch(`http://localhost:4000/api/v1/admin/getoneproduct/${id}`);
+            const data = await respond.json();
+            await setOneProduct(data);             
+        } catch (error) {
+            console.log("Error in fetching content.");
         }
-        fetchOne()
+    }
+    useEffect(()=>{
+        fetchOne();
+        // function for persistent quantity value
+        const callQuantity=()=>{
+            let currentQuantity;
+            const cartArray = JSON.parse(localStorage.getItem("productIds"));
+            const index = cartArray.findIndex(oneObj => oneObj.productId == id );
+            if(index === -1){
+                currentQuantity = 1;
+            }else{
+                currentQuantity = cartArray[index].number;
+                setHandled(true);
+            }
+            setQuantity(currentQuantity);
+        }
+        callQuantity();
     }, [])
     // sizes variables
-    const [size, setSize] = React.useState('');
+    const [size, setSize] = React.useState(12);
     const sizeClothes = [12,14,16,18,20,22,24];
     const handleChangeSize = (event) => {
         setSize(event.target.value);
     };
     // quantity variables
     const [quantity, setQuantity] = useState(1);
-    // Push to Cart function
-    let keys = [];
-    const handleAddCart=(id)=>{
+    // Subsequent handle useState
+    const [handled, setHandled] = useState(false);
+    // Initial push to cart function
+    const handleInitialCart=(id)=>{
         const cartArray = JSON.parse(localStorage.getItem("productIds")) || [];
-        console.log(keys)
-        if (!keys.includes(id)){
-            cartArray.push({"productId":id, "number": 1});
-        }else{
-            const index = cartArray.findIndex(oneObj => oneObj.productId == id );
-            console.log(index)
-            cartArray[index].number += 1;
-        }
+        cartArray.push({"productId":id, "number": quantity});
         localStorage.setItem("productIds", JSON.stringify(cartArray));
-        keys = cartArray.map(key => key.productId);
-        console.log(cartArray);
+        setHandled(true);
+    }
+    // subsequent adding
+    const handleSubsequentAdd=(id)=>{
+        const cartArray = JSON.parse(localStorage.getItem("productIds"));
+        const index = cartArray.findIndex(oneObj => oneObj.productId == id );
+        cartArray[index].number += 1;
+        localStorage.setItem("productIds", JSON.stringify(cartArray));
+    }
+    // susequent removing
+    const handleSubsequentRemove = (id)=>{
+        const cartArray = JSON.parse(localStorage.getItem("productIds"));
+        const index = cartArray.findIndex(oneObj => oneObj.productId == id );
+        cartArray[index].number -= 1;
+        localStorage.setItem("productIds", JSON.stringify(cartArray));   
     }
     return(
         <div>
             <div style={{backgroundColor:"black", marginBottom:"25px"}}>
                 <Nav/>
             </div>
-            {/* {oneProduct && <p>And my name is {oneProduct.product.name}</p> } */}
             {
                 oneProduct &&
                 <div style={{width:"90%", display:"block", margin:"0 auto"}}>
                     <img src={oneProduct.product.image} alt="image of product" style={{height:"50vh", width:"100%", display:"block", margin:"0 auto"}} />
                     <p style={{fontSize:"1.5rem", fontWeight:"500", margin:"10px 0 0 0"}}>{oneProduct.product.name}</p>
                     <p style={{fontSize:"1.3rem", margin:"10px 0 10px 0px"}}>₦{oneProduct.product.price.toLocaleString("en-US")}.00</p>
-                    {/* <p>{oneProduct.product.description}</p> */}
                     <div>
-                        <FormControl fullWidth>
+                        <FormControl fullWidth >
                             <InputLabel id="demo-simple-select-label">Size</InputLabel>
                             <Select
                             labelId="demo-simple-select-label"
@@ -80,16 +99,41 @@ const ProductDescription=()=>{
                         </FormControl>
                     </div>
                     <p style={{fontSize:"1.1rem", margin:"10px 0 10px 0"}}>Quantity</p>
-                    <div style={{ display: "flex", alignItems:"center", justifyContent:"space-between",width:"130px", border:"1px solid #BDBDBD"}}>
-                        <IconButton onClick={() => quantity > 1 && setQuantity(quantity - 1)}>
-                            <RemoveIcon />
-                        </IconButton>
-                        <span style={{fontSize:"1.2rem"}}>{quantity}</span>
-                        <IconButton onClick={() => setQuantity(quantity + 1)}>
-                            <AddIcon />
-                        </IconButton>
+                    { !handled ?
+                    <div>
+                        <div style={{ display: "flex", alignItems:"center", justifyContent:"space-between",width:"130px", border:"1px solid #BDBDBD"}}>
+                            <IconButton onClick={() => quantity > 1 && setQuantity(quantity - 1)}>
+                                <RemoveIcon />
+                            </IconButton>
+                            <span style={{fontSize:"1.2rem"}}>{quantity}</span>
+                            <IconButton onClick={() => setQuantity(quantity + 1)}>
+                                <AddIcon />
+                            </IconButton>
+                        </div>
+                        <div className="filter-bar" onClick={()=>{handleInitialCart(oneProduct.product._id)}} style={{color:"#fff", backgroundColor:"#000", width:"100%", textAlign:"center", padding:"7px", marginTop:"10px"}}><p>Add to Cart</p></div>                   
                     </div>
-                    <div className="filter-bar" onClick={()=>{handleAddCart(oneProduct.product._id)}} style={{color:"#fff", backgroundColor:"#000", width:"100%", textAlign:"center", padding:"7px", marginTop:"10px"}}><p>Add to Cart</p></div>
+                    :
+                    <div>
+                        <div style={{ display: "flex", alignItems:"center", justifyContent:"space-between",width:"130px", border:"1px solid #BDBDBD"}}>
+                            <IconButton onClick={() => {
+                                quantity > 1 && setQuantity(quantity - 1);
+                                handleSubsequentRemove(oneProduct.product._id);
+                            }
+                            }>
+                                <RemoveIcon />
+                            </IconButton>
+                            <span style={{fontSize:"1.2rem"}}>{quantity}</span>
+                            <IconButton onClick={() =>{
+                                setQuantity(quantity + 1)
+                                handleSubsequentAdd(oneProduct.product._id)
+                                } 
+                            }>
+                            <AddIcon />
+                            </IconButton>
+                        </div>   
+                        <Link to="/cart"><div className="filter-bar" style={{color:"#fff", backgroundColor:"#000", width:"100%", textAlign:"center", padding:"7px", marginTop:"10px"}}><p>Go to Cart</p></div></Link>                
+                    </div>
+                    }
                 </div>
             }
 
