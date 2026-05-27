275d7c25246c6d4c4e5445d1ba001b6bcb73abc5
Gifty713 <giftj5584@gmail.com>
2026-05-27 09:50:36 +0100

    recent commit

--- frontend/components/Component/Breif.jsx
+++ frontend/components/Component/Breif.jsx
@@ -11,41 +11,43 @@ const Breif =()=>{
     useEffect(()=>{
         fetchProducts(setProduct);
     }, [])
-    const screenWidth = window.innerWidth
+    const screenWidth = window.innerWidth;
     return(
-        <div style={{position:"relative", width:"100%", zIndex:"3", padding:"0px 0px 0px 0px"}}>
-            <p style={{padding:"0px 0px 0px 13px", fontSize:"1.5rem", fontWeight:"400", letterSpacing:"1px", fontFamily: "Boldonse, system-ui"}}>Best Sellers</p>     
-            {product.length === 0? 
-            <div className="home-clothes-displayed">
-                {
-                    [...Array(screenWidth> 850 ? 8 : 4)].map((item,index)=>{
-                        return(
-                            <div className="home-clothes-img" key={index}>
-                                <Skeleton variant="rectangular" width={"100%"} height={"185px"} animation="wave" />
-                                <Skeleton variant="text" sx={{ fontSize: '1.2rem' }} animation="wave"  />
-                                <Skeleton variant="text" sx={{ fontSize: '1.3rem' }} animation="wave" />
-                            </div>
-                        )
-                    })
-                }
-            </div>
-            : 
+        <div>
+            <div className="breif-cont">
+                <p className="breif-heading" >Best Sellers</p>     
+                {product.length === 0? 
                 <div className="home-clothes-displayed">
-                    {product.slice(0,4).map((prod)=>{
-                        return(
-                            <Link to={`/products/${prod._id}`} style={{textDecoration:"none", color:"black"}} key={prod._id}>
-                                <div className="home-clothes-img" key={prod._id}>
-                                    <img src={prod.image} alt={prod.description} style={{width:"160px", height:"185px", objectFit: "cover", zIndex:"3", boxShadow:"rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px" }} />
-                                    <svg viewBox="0 0 24 24" width="43px" height="43px" fill="none" xmlns="http://www.w3.org/2000/svg" style={{backgroundColor:"#000", padding:"10px", borderRadius:"50%", position:"absolute", top:"50%", left:"68%"}}><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M7.2998 5H22L20 12H8.37675M21 16H9L7 3H4M4 8H2M5 11H2M6 14H2M10 20C10 20.5523 9.55228 21 9 21C8.44772 21 8 20.5523 8 20C8 19.4477 8.44772 19 9 19C9.55228 19 10 19.4477 10 20ZM21 20C21 20.5523 20.5523 21 20 21C19.4477 21 19 20.5523 19 20C19 19.4477 19.4477 19 20 19C20.5523 19 21 19.4477 21 20Z" stroke="#fff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path> </g></svg>
-                                    <p style={{margin:"0px 0px 0px 0px", fontWeight:"500", fontSize:"1.2rem"}}>{prod.name} </p>
-                                    <p style={{margin:"0px 0px 0px 0px", fontSize:"1.3rem", letterSpacing:"0.7px"}}>₦{prod.price.toLocaleString("en-US")}.00</p>
+                    {
+                        [...Array(screenWidth> 850 ? 8 : 4)].map((item,index)=>{
+                            return(
+                                <div className="home-clothes-img" key={index}>
+                                    <Skeleton variant="rectangular" width={"100%"} height={"185px"} animation="wave" />
+                                    <Skeleton variant="text" sx={{ fontSize: '1.2rem' }} animation="wave"  />
+                                    <Skeleton variant="text" sx={{ fontSize: '1.3rem' }} animation="wave" />
                                 </div>
-                            </Link>
-                        )
-                    })}   
-                </div>             
-            }
-            <Link to="/shop"><div className="filter-bar" style={{color:"#fff", backgroundColor:"#000", display:"block", margin:"0 auto",width:"70%", height:"60px", textAlign:"center", padding:"7px", marginTop:"10px", borderRadius:"10px"}}><p style={{marginTop:"10px"}}>SEE MORE</p></div></Link>
+                            )
+                        })
+                    }
+                </div>
+                : 
+                    <div className="home-clothes-displayed">
+                        {product.slice(0,screenWidth> 850 ? 8 : 4).map((prod)=>{
+                            return(
+                                <Link to={`/products/${prod._id}`} style={{textDecoration:"none", color:"black"}} key={prod._id}>
+                                    <div className="home-clothes-img" key={prod._id}>
+                                        <img src={prod.image} alt={prod.description}  style={{width:"160px", height:"185px", objectFit: "cover", zIndex:"3", boxShadow:"rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px" }} />
+                                        <svg viewBox="0 0 24 24" width="25px" height="25px" className="cart-svg-img" fill="none" xmlns="http://www.w3.org/2000/svg" ><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M7.2998 5H22L20 12H8.37675M21 16H9L7 3H4M4 8H2M5 11H2M6 14H2M10 20C10 20.5523 9.55228 21 9 21C8.44772 21 8 20.5523 8 20C8 19.4477 8.44772 19 9 19C9.55228 19 10 19.4477 10 20ZM21 20C21 20.5523 20.5523 21 20 21C19.4477 21 19 20.5523 19 20C19 19.4477 19.4477 19 20 19C20.5523 19 21 19.4477 21 20Z" stroke="#fff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path> </g></svg>
+                                        <p className="product-name-breif">{prod.name} </p>
+                                        <p className="product-price-breif" >₦{prod.price.toLocaleString("en-US")}.00</p>
+                                    </div>
+                                </Link>
+                            )
+                        })}   
+                    </div>             
+                }
+                <Link to="/shop"><div className="more-bar"><p>SEE MORE</p></div></Link>
+            </div>
             <Footer/>
         </div>
     )
--- frontend/components/Component/Hero.jsx
+++ frontend/components/Component/Hero.jsx
@@ -1,7 +1,8 @@
 import "./ComponentStyle.css";
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay } from 'swiper/modules';
+import { EffectCoverflow, Autoplay } from "swiper/modules";
 import { fetchProducts } from "./FetchProducts.jsx";
+import "swiper/css/effect-coverflow";
 import 'swiper/css';
 import Nav from "./Nav.jsx";
 import { useEffect, useState } from "react";
@@ -16,18 +17,43 @@ const Hero=()=>{
         <div >
             <Nav/>
             <div className="hero-cont">
-                <Swiper
-                modules={[Autoplay]}
-                autoplay={{delay: 2000, disableOnInteraction: false, }}
-                loop={products.length > 0} 
-                >                
-                    {screenWidth > 850 ? <SwiperSlide><img src="/bg-laptop.png" alt="Image of swiped product" className="sliding-images"/></SwiperSlide> : <SwiperSlide><img src="/bg_mobilee.png" alt="Image of swiped product" className="sliding-images"/></SwiperSlide> }
-                    {products.slice(0, screenWidth > 850 ? 3: 7).map((product)=>{
-                        return(
-                            <SwiperSlide><img src={product.image} className="sliding-images" alt="Image of swiped product"/></SwiperSlide>                      
-                        )
-                    })}       
-                </Swiper>          
+                {screenWidth < 850 ?
+                    <Swiper
+                        modules={[Autoplay]}
+                        autoplay={{delay: 2000, disableOnInteraction: false, }}
+                        loop={products.length > 0} 
+                    >                
+                        <SwiperSlide><img src="/bg_mobilee.png" alt="Image of swiped product" className="sliding-images"/></SwiperSlide> 
+                        {products.slice(0, 3).map((product)=>{
+                            return(
+                                <SwiperSlide><img src={product.image} className="sliding-images" alt="Image of swiped product"/></SwiperSlide>                      
+                            )
+                        })}       
+                    </Swiper>  
+                :
+                    <Swiper
+                        autoplay={{delay: 2000, disableOnInteraction: false, }}
+                        slidesPerView={2}
+                        centeredSlides={true}
+                        effect="coverflow"
+                        modules={[Autoplay, EffectCoverflow]}
+                        coverflowEffect={{
+                            rotate: 0,
+                            stretch: 0,
+                            depth: 100,
+                            modifier: 2,
+                            slideShadows: false,
+                            scale: 1,
+                        }}
+                    >                
+                        <SwiperSlide><img src="/bg_mobilee.png" alt="Image of swiped product" className="sliding-images"/></SwiperSlide> 
+                        {products.slice(0, 7).map((product)=>{
+                            return(
+                                <SwiperSlide><img src={product.image} className="sliding-images" alt="Image of swiped product"/></SwiperSlide>                      
+                            )
+                        })}       
+                    </Swiper>                              
+                }
             </div>
         </div>
     )
